570a6a13c1a41e55372c6481ae7a934cdcc19ab1
smeek123 <sean.meek06@gmail.com>
2022-10-07 17:16:57 -0400

    Initial Commit

---
+++ MusicSharing/MusicSharingApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicSharingApp.swift
+//  MusicSharing
+//
+//  Created by Sean P. Meek on 10/7/22.
+//
+
+import SwiftUI
+
+@main
+struct MusicSharingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
